94428c34f4b17786b4d55087bdeee866ef47df76
brooke-stanley-agina <brooksville01@gmail.com>
2021-06-19 12:20:00 +0300

    python basics

---
+++ app.py
@@ -0,0 +1,11 @@
+
+# character_name = input('Enter your name: ')
+# character_age = input('Enter your age: ')
+# bool  = False
+
+# print('My name is: ' + character_name)
+# print('I am: ' + character_age + 'yrs old')
+# print('I like the name ' + character_name)
+# print('I like being ' + str(character_age) +'yrs old')
+# print('ismale: ' + str(bool))
+
